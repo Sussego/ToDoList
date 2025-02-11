908a3933c3f840a2bfc1fc40ecf85282fb3fd37a
Sussego <ccedu.uenp@gmail.com>
2025-02-11 12:48:50 -0300

    Add and remove topic

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,37 @@ import { useState } from "react";
 function App() {
   const [tarefas,setTarefas] = useState([]);
   const [novaTarefa, setNovaTarefa] = useState('');
+
+  const adicionarTarefa = () =>{
+    if (novaTarefa.trim() !== ''){
+      setTarefas([...tarefas, novaTarefa]);
+      setNovaTarefa('');
+    }
+  };
+
+  const removerTarefa = (index)=>{
+    const novasTarefas = tarefas.filter((_,i)=>i !== index);
+    setTarefas(novasTarefas)
+  };
+
+
   return (
-    <div style={{textAlign: 'center', marginTop: '50px'}}>
+    <div style={{textAlign: 'center', marginTop: '50px', display: 'flex', justifyContent:'center', alignItems:'center'}}>
       <h1>Lista de Tarefas</h1>
+        <input
+          type="text"
+          placeholder="Digite uma Tarefa"
+          value={novaTarefa}
+          onChange={(e)=>setNovaTarefa(e.target.value)}
+        />
+        
+        <button on onClick={adicionarTarefa}>Adicionar</button>
+
+        <ul>
+          {tarefas.map((tarefa, index)=> (
+            <li key={index} onClick={()=>removerTarefa(index)} style={{cursor:'pointer'}}>{tarefa}</li>
+          ))}
+        </ul>
     </div>
   );
 }
